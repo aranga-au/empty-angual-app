a4cedeb936549383e91ac7061d6296644ae7cb0b
Aranga C. Nanayakkara <aranga@outlook.com.au>
2016-02-14 16:53:30 +1100

    add package json and bower.json. inital gulp files for building

---
+++ src/server/data.js
@@ -0,0 +1,14 @@
+module.exports = {
+    people: getPeople()
+};
+
+function getPeople() {
+    return [
+        {id: 1, firstName: 'Aranga', lastName: 'Nanayakkara',  location: 'Taylors Hill'},
+        {id: 2, firstName: 'Nirasha', lastName: 'Unawatuna',  location: 'Taylors Hill'},
+        {id: 3, firstName: 'Peter', lastName: 'Martin',  location: 'Montrose'},
+        {id: 4, firstName: 'Daragh', lastName: 'Martin', location: 'Carlton'},
+        {id: 5, firstName: 'Monica', lastName: 'Chand', location: 'North Melbourne'},
+        {id: 6, firstName: 'Amalia', lastName: 'O\'Neil', location: 'North Melbourne'}
+    ];
+}
---
+++ src/server/readme.txt
@@ -0,0 +1,1 @@
+just to test inital proto types
